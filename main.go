9d5a0fa706c580337724f3401a4848c803fcaa3b
hisshihi <hissoffc@gmail.com>
2025-03-12 23:44:35 +0500

    Оптимизирован код калькулятора инвестиционной доходности: вынесены переменные в глобальную область видимости и добавлены функции для расчета будущей стоимости и ввода данных.

--- main.go
+++ main.go
@@ -5,26 +5,37 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5 // Уровень инфляций
+var (
+	investmentAmount   float64 = 1000.0 // Сумма инвестиций
+	expectedReturnRate float64 = 5.5 // Ожидаемая доходность
+	years              float64 = 10.0 // Кол-во лет
+)
+
 func main() {
-	const inflationRate = 2.5 // Уровень инфляций
-	var investmentAmount float64 // Сумма инвестиций
-	var expectedReturnRate float64 // Ожидаемая доходность
-	var years float64 // Кол-во лет
+	askAQuestion()
 
-	fmt.Print("Введите сумму инвестиций: ")
-	fmt.Scan(&investmentAmount)
-	
-	fmt.Print("Ввидите ожидаемую доходность: ")
-	fmt.Scan(&expectedReturnRate)
-
-	fmt.Print("Ввиде кол-во лет: ")
-	fmt.Scan(&years)
-
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValue()
 
 	formattedFV := fmt.Sprintf("Future value %.1f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future value (adjusted for Inflation): %.1f\n", futureRealValue)
 
 	fmt.Print(formattedFV, formattedRFV)
+}
+
+func calculateFutureValue() (fv, frv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	frv = fv / math.Pow(1+inflationRate/100, years)
+	return
+}
+
+func askAQuestion() {
+	fmt.Print("Введите сумму инвестиций: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Ввидите ожидаемую доходность: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Ввиде кол-во лет: ")
+	fmt.Scan(&years)
 }
