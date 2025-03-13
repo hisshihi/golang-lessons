7b78bd7270f383e2ac0e52a1175adb673d706d80
hisshihi <hissoffc@gmail.com>
2025-03-13 19:15:04 +0500

    Добавлена возможность ведения истории операций: реализованы функции для чтения и записи операций в файл, обновлено меню с добавлением пункта для отображения истории операций.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 
 	"golang.org/x/text/language"
@@ -13,18 +15,67 @@ var (
 	min         float64 = 0.00
 	max         float64 = 100000.00
 	userBalance         = min + rand.Float64()*(max-min)
-	questions   string  = "1. Проверить баланс\n2. Внести наличные\n3. Снять наличные\n4. Выйти\n\n"
+	questions   string  = "1. Проверить баланс\n2. Внести наличные\n3. Снять наличные\n4. Выйти\n5. История операций\n\n"
+	fileName            = "operations.txt"
 )
+
+func readOperationsFromFile() []string {
+	operations := []string{}
+
+	// Проверяем существование файла
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		fmt.Println("История операций пуста")
+		return operations
+	}
+
+	// Читаем содержимое файла
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Ошибка при чтении файла:", err)
+		return operations
+	}
+
+	// Разбиваем содержимое на строки
+	operations = strings.Split(string(data), "\n")
+
+	// Удаляем пустую строку в конце, если она есть
+	if operations[len(operations)-1] == "" {
+		operations = operations[:len(operations)-1]
+	}
+
+	return operations
+}
+
+func writeOperationToFile(operation string, cash string, balance string) {
+	userOperation := fmt.Sprintf("%v%v. %v. Время -> %v\n", operation, cash, balance, time.Now())
+
+	// Открываем файл с флагами для добавления записей (O_APPEND),
+	// создания если не существует (O_CREATE) и записи (O_WRONLY)
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Записываем новую операцию в файл
+	if _, err := file.WriteString(userOperation); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	sayHello()
 
 	var choice int
-	for choice != 4 {
+	for {
 		fmt.Println(questions)
 		fmt.Print("Выберите вариант из предложенных: ")
 		fmt.Scan(&choice)
 
+		if choice == 4 {
+			fmt.Println("До свидания!")
+			break
+		}
 		userChoice(choice)
 	}
 }
@@ -48,7 +99,7 @@ func userChoice(choice int) {
 
 	switch choice {
 	case 1:
-		p.Printf("Ваш баланс: %.2f \n", userBalance)
+		p.Printf("Ваш баланс: %.2f₽ \n", userBalance)
 	case 2:
 		fmt.Print("Сколько вы хотите внести? ")
 		fmt.Scan(&cash)
@@ -56,7 +107,8 @@ func userChoice(choice int) {
 			fmt.Println("Неверная сумма для пополнения")
 		} else {
 			userBalance += cash
-			p.Printf("Ваш баланс: %.2f\n", userBalance)
+			p.Printf("Ваш баланс: %.2f₽\n", userBalance)
+			writeOperationToFile("Пополнение счёта -> ", p.Sprintf("%.2f₽", cash), p.Sprintf("Текущий баланс -> %.2f₽", userBalance))
 		}
 	case 3:
 		fmt.Print("Сколько вы хотите снять? ")
@@ -65,10 +117,19 @@ func userChoice(choice int) {
 			fmt.Println("Неверная сумма для снятия")
 		} else if userBalance >= cash {
 			userBalance -= cash
-			p.Printf("Ваш баланс: %.2f\n", userBalance)
+			p.Printf("Ваш баланс: %.2f₽\n", userBalance)
+			writeOperationToFile("Снятие со счёта -> ", p.Sprintf("%.2f₽", cash), p.Sprintf("Текущий баланс -> %.2f₽", userBalance))
 		} else {
 			fmt.Println("Недостаточно средств")
 		}
+	case 5:
+		fmt.Println("\nИстория операций:")
+		fmt.Println("------------------------")
+		operations := readOperationsFromFile()
+		for _, operation := range operations {
+			fmt.Println(operation)
+		}
+		fmt.Println("------------------------")
 	default:
 		fmt.Println("Неверный ввод")
 	}
