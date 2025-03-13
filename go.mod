92260e7ab2ab634049faa80079bf9db8a5307ae7
hisshihi <hissoffc@gmail.com>
2025-03-13 15:23:09 +0500

    Изменен модуль на bank, добавлена зависимость golang.org/x/text v0.23.0. Обновлено взаимодействие с пользователем: добавлены функции для приветствия и управления балансом, изменена логика выбора действий.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/hisshihi/golang-lessons
+module github.com/hisshihi/bank
 
 go 1.23.6
+
+require golang.org/x/text v0.23.0
--- main.go
+++ main.go
@@ -2,33 +2,64 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+var (
+	min         float64 = 0.00
+	max         float64 = 100000.00
+	userBalance         = min + rand.Float64()*(max-min)
+	questions   string  = "1. Проверить баланс\n2. Внести наличные\n3. Снять наличные\n4. Выйти\n\n"
 )
 
 func main() {
-	revenue := getUserInput("Доход: ")
-	expenses := getUserInput("Расходы: ")
-	taxRate := getUserInput("Налоговая ставка: ")
+	sayHello()
 
-	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+	var choice int
+	for choice != 4 {
+		fmt.Println(questions)
+		fmt.Print("Выберите вариант из предложенных: ")
+		fmt.Scan(&choice)
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Printf("Ratio: %.2f\n", ratio)
+		userChoice(choice)
+	}
 }
 
-func calculateFinancials(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
-	ebt = revenue - expenses
-	profit = ebt * (1 - taxRate / 100)
-	ratio = ebt / profit
-
-	return
+func sayHello() {
+	now := time.Now().Hour()
+	if now >= 23 && now <= 6 {
+		fmt.Println("Доброй ночи!")
+	} else if now > 6 && now <= 11 {
+		fmt.Println("Доброе утро!")
+	} else if now > 11 && now <= 16 {
+		fmt.Println("Добрый день!")
+	} else {
+		fmt.Println("Добрый вечер!")
+	}
 }
 
-func getUserInput(infoText string) float64 {
-	var userInput float64
+func userChoice(choice int) {
+	var cash float64
+	p := message.NewPrinter(language.Russian)
 
-	fmt.Print(infoText)
-	fmt.Scan(&userInput)
-
-	return userInput
+	switch choice {
+	case 1:
+		p.Printf("Ваш баланс: %.2f \n", userBalance)
+	case 2:
+		fmt.Print("Сколько вы хотите внести? ")
+		fmt.Scan(&cash)
+		userBalance += cash
+		p.Printf("Ваш баланс: %.2f\n", userBalance)
+	case 3:
+		fmt.Print("Сколько вы хотите снять? ")
+		fmt.Scan(&cash)
+		userBalance -= cash
+		p.Printf("Ваш баланс: %.2f\n", userBalance)
+	default:
+		fmt.Println("Неверный ввод")
+	}
 }
