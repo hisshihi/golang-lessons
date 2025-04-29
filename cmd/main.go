f6e47a4487fd4950d55d036a2e2724ca5688e35f
hisshihi <hissoffc@gmail.com>
2025-04-29 14:36:32 +0500

    Изучить работу с указателями и структурами. Начать изучение методов(Receiver)

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	"github.com/hisshihi/golang-lessons/internal/banking"
-	"github.com/hisshihi/golang-lessons/pkg/utils"
+	"fmt"
+	"time"
 )
 
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
 func main() {
-	utils.SayHello()
-	banking.StartApp()
+	appUser := &user{
+		firstName: getUserString("Enter your first name:"),
+		lastName:  getUserString("Enter your last name:"),
+		birthDate: getUserString("Enter your birth date (YYYY-MM-DD):"),
+		createdAt: time.Now(),
+	}
+
+	appUser.outputUserDetails()
+}
+
+// Receiver
+func (u *user) outputUserDetails() {
+	fmt.Printf("First Name: %s\n", u.firstName)
+	fmt.Printf("Last Name: %s\n", u.lastName)
+	fmt.Printf("Birth Date: %s\n", u.birthDate)
+	fmt.Printf("User created at %v\n", u.createdAt.Format("2006-01-02 15:04:05"))
+}
+
+func getUserString(promt string) string {
+	fmt.Println(promt)
+	var input string
+	fmt.Scanln(&input)
+
+	return input
 }
