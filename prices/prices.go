6638592a9224ed0cd7c16cb52cbf0a80d3a7ef05
hisshihi <hissoffc@gmail.com>
2025-08-12 13:57:14 +0500

    Добавить считываение из файла. Доработать логику

--- prices/prices.go
+++ prices/prices.go
@@ -1,7 +1,12 @@
 // Package prices provides functionality related to pricing calculations.
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -9,10 +14,48 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) LoadData() error {
+	// Загружаем файл и получаем данные о ценах
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		return fmt.Errorf("возникла ошибка с чтением файла: %v", err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return fmt.Errorf("возникла ошибка с чтением данных из файла: %v", err.Error())
+	}
+
+	prices := make([]float64, len(lines))
+	for lineIndex, line := range lines {
+		price, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			return fmt.Errorf("не удалось преобразовать строку в float: %v", err.Error())
+		}
+
+		prices[lineIndex] = price
+	}
+
+	job.InputPrices = prices
+
+	return nil
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
+
+	result := make(map[string]string)
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
