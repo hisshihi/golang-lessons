f22f4ffc26c1dacb112d0f47ac64140d8e68f32a
hisshihi <hissoffc@gmail.com>
2025-09-21 15:59:08 +0500

    Добавить цель для тестирования в Makefile и удалить код калькулятора из main.go

--- main.go
+++ main.go
@@ -1,44 +1,4 @@
 package main
 
-import "fmt"
-
-// создать обычный калькулятор, где iota - это операции + - * /
-
-const (
-	ADD = iota
-	SUB
-	MUL
-	DIV
-)
-
-type Operation int
-
-func (op Operation) Calculate(a, b float64) float64 {
-	switch op {
-	case ADD:
-		return a + b
-	case SUB:
-		return a - b
-	case MUL:
-		return a * b
-	case DIV:
-		if b != 0 {
-			return a / b
-		}
-		panic("division by zero")
-	default:
-		panic("unknown operation")
-	}
-}
-
 func main() {
-	var op Operation
-	op = ADD
-	fmt.Println(op.Calculate(5, 3))
-	op = SUB
-	fmt.Println(op.Calculate(5, 3))
-	op = MUL
-	fmt.Println(op.Calculate(5, 3))
-	op = DIV
-	fmt.Println(op.Calculate(5, 3))
 }
