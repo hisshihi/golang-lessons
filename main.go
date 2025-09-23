1b12a7632f8fe7e54ee2daebc33fe6d2efbb5eb2
hisshihi <hissoffc@gmail.com>
2025-09-23 12:50:38 +0500

    Добавить функциональность для суммирования чисел, введенных пользователем через пробелы

--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	fmt.Print("Please, enter numbers separated by spaces: ")
+	r := bufio.NewReader(os.Stdin)
+
+	sum := 0
+
+	for {
+		input, inputErr := r.ReadString(' ')
+		n := strings.TrimSpace(input)
+		if n == "" {
+			continue
+		}
+
+		num, numErr := strconv.Atoi(n)
+		if numErr != nil {
+			fmt.Println(numErr)
+		} else {
+			sum += num
+		}
+
+		if inputErr == io.EOF {
+			break
+		}
+		if inputErr != nil {
+			fmt.Println(inputErr)
+		}
+	}
+	fmt.Println(sum)
 }
