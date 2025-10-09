1b51ac4a6cd7793d21003622e6b1e934b71f51ce
hisshihi <hissoffc@gmail.com>
2025-10-09 23:29:59 +0500

    Добавить функциональность для загрузки, кодирования и сохранения изображений в формате webp

--- main.go
+++ main.go
@@ -1,31 +1,110 @@
 package main
 
-import "log"
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"image"
+	_ "image/gif"  // Регистрация GIF декодера
+	_ "image/jpeg" // Регистрация JPEG декодера
+	_ "image/png"  // Регистрация PNG декодера
+	"log"
+	"os"
+	"strings"
 
-type Cache[K comparable, V any] struct {
-	data map[K]V
+	"github.com/chai2010/webp"
+	"github.com/google/uuid"
+)
+
+var fileNames []string = []string{
+	"SCR-20251008-qxbq.png",
+	"SCR-20251008-qxnj.png",
+	"SCR-20251008-qxsj.png",
 }
 
-func NewCache[K comparable, V any]() *Cache[K, V] {
-	return &Cache[K, V]{make(map[K]V)}
+func makeWork[I any](base64Images ...I) <-chan I {
+	out := make(chan I)
+	go func() {
+		defer close(out)
+		for _, img := range base64Images {
+			out <- img
+		}
+	}()
+	return out
 }
 
-func (c *Cache[K, V]) Add(key K, value V) {
-	c.data[key] = value
+func pipline[I any, O any](quit <-chan struct{}, input <-chan I, worker func(I) O) <-chan O {
+	out := make(chan O, 3)
+	go func() {
+		defer close(out)
+		for i := range input {
+			select {
+			case out <- worker(i):
+			case <-quit:
+				return
+			}
+		}
+	}()
+	return out
 }
 
-func (c *Cache[K, V]) Get(key K) (V, bool) {
-	value, ok := c.data[key]
-	return  value, ok
+// Закгрузка изображений с диска
+func loadImage(paths string) []byte {
+	data, err := os.ReadFile(paths)
+	if err != nil {
+		log.Printf("Error reading file %s: %v", paths, err)
+		return nil
+	}
+	return data
+}
+
+// Кодируем изображения в base64
+func encodeBase64(image []byte) string {
+	encoded := base64.StdEncoding.EncodeToString(image)
+	return encoded
+}
+
+// Декодируем base64 в изображение
+func decodeBase64(encoded string) image.Image {
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(encoded))
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		log.Printf("Error decoding base64 string: %v", err)
+		return nil
+	}
+	return img
+}
+
+// Преобразование изображений в формат webp
+func encodeToWeb(img image.Image) bytes.Buffer {
+	var buf bytes.Buffer
+	if err := webp.Encode(&buf, img, &webp.Options{Lossless: true}); err != nil {
+		log.Fatal(err)
+	}
+	return buf
+}
+
+// Сохранение изображения на диск
+func saveToDisk(buf bytes.Buffer) {
+	fileName := fmt.Sprintf("image_%s.webp", uuid.New().String())
+	if err := os.WriteFile(fileName, buf.Bytes(), os.FileMode(0o644)); err != nil {
+		log.Printf("Error saving file %s: %v", fileName, err)
+	}
+	fmt.Println(fileName)
 }
 
 func main() {
-	cache := NewCache[any, any]()
+	quit := make(chan struct{})
+	var signal struct{}
 
-	cache.Add("", "value")
-	value, ok := cache.Get("")
-	if !ok {
-		log.Fatal("value not found")
+	images := makeWork(fileNames...)
+	loadedImages := pipline(quit, images, loadImage)
+	encodedImages := pipline(quit, loadedImages, encodeBase64)
+	quit <- signal
+	decodedImages := pipline(quit, encodedImages, decodeBase64)
+	webpImages := pipline(quit, decodedImages, encodeToWeb)
+
+	for webpImg := range webpImages {
+		saveToDisk(webpImg)
 	}
-	log.Println(value)
 }
