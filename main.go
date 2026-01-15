0ae093da65957bd28a2a5e4713c7b94b740dbb9d
hisshihi <hissoffc@gmail.com>
2026-01-15 22:05:56 +0500

    Добавить функцию postman для обработки новостей с использованием WaitGroup

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func postman(text string, countPostman int) {
+	for i := range countPostman {
+		fmt.Printf("Я почтальён. Я отнёс газету %s в %d раз\n", text, i+1)
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+
+	namesNews := []string{"Новости", "IT и технологии", "Игровые новости"}
+
+	for _, name := range namesNews {
+		wg.Go(func() {
+			postman(name, len(namesNews))
+		})
+	}
+	wg.Wait()
 }
