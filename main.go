fccbf8145078373fbcc5acea902248a20e561ac9
hisshihi <hissoffc@gmail.com>
2026-02-19 17:54:04 +0500

    Упрощение main.go: удалены неиспользуемые функции и контекст, изменена логика обработки данных, добавлен вывод времени выполнения.

--- main.go
+++ main.go
@@ -1,112 +1,39 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
-type outVal struct {
-	val int
-	err error
-}
+var maxWaitSeconds = 5
 
-var errTimeout = errors.New("timed out")
+// randomWait вызов долго работающей функции
+func randomWait() int {
+	workSeconds := rand.Intn(maxWaitSeconds + 1)
 
-func processData(ctx context.Context, val int) chan outVal {
-	ch := make(chan int)
-	out := make(chan outVal)
+	time.Sleep(time.Duration(workSeconds) * time.Second)
 
-	go func() {
-		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-		close(ch)
-	}()
-
-	go func() {
-		select {
-		case <-ch:
-			out <- outVal{
-				val: val * 2,
-				err: nil,
-			}
-		case <-ctx.Done():
-			out <- outVal{
-				val: 0,
-				err: errTimeout,
-			}
-		}
-	}()
-
-	return out
+	return workSeconds
 }
 
 func main() {
-	inCh := make(chan int)
-	outCh := make(chan int)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	go func() {
-		defer close(inCh)
-		for i := range 10 {
-			select {
-			case inCh <- i + 1:
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-
+	totalWorkSeconds := 0
 	now := time.Now()
-	processParallel(ctx, inCh, outCh, 5)
 
-	for val := range outCh {
-		fmt.Println(val)
+	ch := make(chan int)
+
+	for range 100 {
+		go func() {
+			workSeconds := randomWait()
+			ch <- workSeconds
+		}()
 	}
+ 
+	for range 100 {
+		totalWorkSeconds += <- ch
+	}
+
 	fmt.Println(time.Since(now))
-}
-
-func processParallel(ctx context.Context, inCh <-chan int, outCh chan<- int, workerCount int) {
-	wg := &sync.WaitGroup{}
-	for range workerCount {
-		wg.Add(1)
-		go worker(ctx, inCh, outCh, wg)
-	}
-
-	go func() {
-		wg.Wait()
-		close(outCh)
-	}()
-}
-
-func worker(ctx context.Context, inCh <-chan int, outCh chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for {
-		select {
-		case val, ok := <-inCh:
-			if !ok {
-				return
-			}
-			select {
-			case ov := <-processData(ctx, val):
-				if ov.err != nil {
-					return
-				}
-				select {
-				case outCh <- ov.val:
-				case <-ctx.Done():
-					return
-				}
-			case <-ctx.Done():
-				return
-			}
-		case <-ctx.Done():
-			return
-		}
-	}
+	fmt.Println("work seconds:", totalWorkSeconds)
 }
