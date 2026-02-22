767dc45439f162f52f1e6d64bf42834a01ade8fb
hisshihi <hissoffc@gmail.com>
2026-02-22 14:25:15 +0500

    feat: Добавить обработку ожидающих сессий. Свитчинг и воркинг сессий если их взяли или не взяли в работу

--- main.go
+++ main.go
@@ -1,39 +1,104 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"math/rand"
+	"sync"
 	"time"
 )
 
-var maxWaitSeconds = 5
+type Session struct {
+	ID          int
+	ExitSeconds int
+	IsWorking   bool
+	IsSwitched  bool
+	// mu для защиты работы от конкурентных вызовов
+	mu sync.Mutex
+	// workCh канал для сигнализации о том, что сессия начала работу
+	workCh      chan struct{}
+	CountSwitch int
+}
 
-// randomWait вызов долго работающей функции
-func randomWait() int {
-	workSeconds := rand.Intn(maxWaitSeconds + 1)
+func NewSession(id, seconds int) *Session {
+	return &Session{
+		ID:          id,
+		ExitSeconds: seconds,
+		workCh:      make(chan struct{}),
+	}
+}
 
-	time.Sleep(time.Duration(workSeconds) * time.Second)
+func (s *Session) Work() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-	return workSeconds
+	if s.IsWorking {
+		return
+	}
+	s.IsWorking = true
+	close(s.workCh)
+	fmt.Printf("Сессия %d начала работу\n", s.ID)
+}
+
+func (s *Session) Switch() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.IsWorking {
+		return
+	}
+
+	s.IsSwitched = true
+	s.CountSwitch++
+	fmt.Printf("Сессия %d переключена\n", s.ID)
+}
+
+// WorkerSession обрабатывает сессии. Если сессию не взяли в работу за период её жизни, то она переключается в режим работы
+func WorkerSession(ctx context.Context, session <-chan *Session) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Воркер завершён")
+			return
+		case s, ok := <-session:
+			if !ok {
+				return
+			}
+			go monitorSession(s)
+		}
+	}
+}
+
+func monitorSession(s *Session) {
+	ticker := time.NewTicker(time.Duration(s.ExitSeconds) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-s.workCh:
+			return
+		case <-ticker.C:
+			s.Switch()
+		}
+	}
 }
 
 func main() {
-	totalWorkSeconds := 0
-	now := time.Now()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	ch := make(chan int)
+	sessionStream := make(chan *Session)
+	go WorkerSession(ctx, sessionStream)
 
-	for range 100 {
-		go func() {
-			workSeconds := randomWait()
-			ch <- workSeconds
-		}()
-	}
- 
-	for range 100 {
-		totalWorkSeconds += <- ch
-	}
+	s1 := NewSession(1, 1)
+	sessionStream <- s1
 
-	fmt.Println(time.Since(now))
-	fmt.Println("work seconds:", totalWorkSeconds)
+	s2 := NewSession(2, 3)
+	sessionStream <- s2
+
+	time.Sleep(3 * time.Second)
+	s1.Work()
+
+	time.Sleep(10 * time.Second)
+	fmt.Printf("session 1: %#v\n", s1)
+	fmt.Printf("session 2: %#v\n", s2)
 }
