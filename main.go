6f4b0f4a2e33766cd343149dd382f2ea46703ed9
hisshihi <hissoffc@gmail.com>
2026-03-03 00:00:16 +0500

    feat: добавить реализацию отмены выполнения через контекст в случае выполнения функции больше заданного лимита

--- main.go
+++ main.go
@@ -3,103 +3,54 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync"
+	"log"
+	"math/rand"
 	"time"
 )
 
-type Session struct {
-	ID          int
-	ExitSeconds int
-	IsWorking   bool
-	IsSwitched  bool
-	// mu для защиты работы от конкурентных вызовов
-	mu sync.Mutex
-	// workCh канал для сигнализации о том, что сессия начала работу
-	workCh      chan struct{}
-	CountSwitch int
-}
+func rpcCall(ctx context.Context) (string, error) {
+	duration := time.Duration(rand.Intn(10)+1) * time.Second
+	fmt.Println("duration", duration)
+	timer := time.NewTimer(duration)
+	defer timer.Stop()
 
-func NewSession(id, seconds int) *Session {
-	return &Session{
-		ID:          id,
-		ExitSeconds: seconds,
-		workCh:      make(chan struct{}),
+	select {
+	case <-timer.C:
+		return "data", nil
+	case <-ctx.Done():
+		return "", fmt.Errorf("rpc error: %w", ctx.Err())
 	}
 }
 
-func (s *Session) Work() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+func callRPCWithTimeout(ctx context.Context, timeout time.Duration) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
 
-	if s.IsWorking {
-		return
-	}
-	s.IsWorking = true
-	close(s.workCh)
-	fmt.Printf("Сессия %d начала работу\n", s.ID)
-}
-
-func (s *Session) Switch() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if s.IsWorking {
-		return
+	type result struct {
+		data string
+		err  error
 	}
 
-	s.IsSwitched = true
-	s.CountSwitch++
-	fmt.Printf("Сессия %d переключена\n", s.ID)
-}
+	ch := make(chan result, 1)
+	go func() {
+		data, err := rpcCall(ctx)
+		ch <- result{data: data, err: err}
+	}()
 
-// WorkerSession обрабатывает сессии. Если сессию не взяли в работу за период её жизни, то она переключается в режим работы
-func WorkerSession(ctx context.Context, session <-chan *Session) {
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Воркер завершён")
-			return
-		case s, ok := <-session:
-			if !ok {
-				return
-			}
-			go monitorSession(s)
-		}
-	}
-}
-
-// monitorSession отслеживает сессию и переключает её в режим работы
-func monitorSession(s *Session) {
-	ticker := time.NewTicker(time.Duration(s.ExitSeconds) * time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-s.workCh:
-			return
-		case <-ticker.C:
-			s.Switch()
-		}
+	select {
+	case res := <-ch:
+		return res.data, res.err
+	case <-ctx.Done():
+		return "", fmt.Errorf("timeout: %w", ctx.Err())
 	}
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	sessionStream := make(chan *Session)
-	go WorkerSession(ctx, sessionStream)
-
-	s1 := NewSession(1, 1)
-	sessionStream <- s1
-
-	s2 := NewSession(2, 3)
-	sessionStream <- s2
-
-	time.Sleep(3 * time.Second)
-	s1.Work()
-
-	time.Sleep(10 * time.Second)
-	fmt.Printf("session 1: %#v\n", s1)
-	fmt.Printf("session 2: %#v\n", s2)
+	ctx := context.Background()
+	data, err := callRPCWithTimeout(ctx, 5*time.Second)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+	fmt.Println(data)
 }
