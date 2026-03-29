dc57729cfc1aa8a63001aac66a677e8c243b9764
hisshihi <hissoffc@gmail.com>
2026-03-29 22:52:03 +0500

    test

--- main.go
+++ main.go
@@ -1,48 +1,19 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
+import "log"
 
-	"golang.org/x/sync/errgroup"
-)
-
-var urls = []string{"https://example.com", "https://example.org", "https://example.net"}
-
-func main() {
-	ctx := context.Background()
-	g, qctx := errgroup.WithContext(ctx)
-	g.SetLimit(2)
-	for _, url := range urls {
-		g.Go(func() error {
-			return isAvailable(qctx, url)
-		})
-	}
-
-	if err := g.Wait(); err != nil {
-		log.Fatalf("Some resource is not available: %v", err)
-	} else {
-		log.Println("All resource available")
-	}
+type User struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
 }
 
-func isAvailable(ctx context.Context, url string) error {
-	c := http.Client{}
-	req, err := http.NewRequestWithContext(ctx, "OPTIONS", url, nil)
-	if err != nil {
-		return err
-	}
+func hello() string {
+	hello := "hello"
+	user := User{ID: 1, Name: "hiss"}
+	log.Println(user)
+	return hello
+}
 
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("wrong status code %d for url %v", resp.StatusCode, url)
-	}
-
-	return nil
+func main() {
+	hello()
 }
